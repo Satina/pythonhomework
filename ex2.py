0e09c61c350400221e4977c7db3f9c36919bad07
satina <satina@materiamagica.com>
2015-03-03 06:57:25 -0500

    two dot lesson

---
+++ ex2.py
@@ -0,0 +1,12 @@
+print "So I am supposed to practice comments"
+
+# "This is a comment"
+print "This is not a comment"
+
+# print "This is also a comment"
+
+print "If you're nosing in here"
+print "You deserve to know"
+
+# print "Skunk + bike = DISASTER"
+print "That is all thanks"
